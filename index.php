7387f19e0bac5e87e3273e79d6e7a0be673ac3ff
Danborusan <88danang88@gmail.com>
2016-10-18 14:38:59 +0700

    Penambahan Profile Page

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
    </script>
    -->
 
-   <nav class="navbar navbar-default" style="background-color: #1DA1F2;>
+   <nav class="navbar navbar-default" style="background-color: #BCDEE7;>
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -57,19 +57,19 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#" style="color: #fff">Twikker</a>
+      <a class="navbar-brand" href="#" style="color: #000">Twikker</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="#" style="color: #fff">Wall<span class="sr-only">(current)</span></a></li>
-        <li><a href="#" style="color: #fff">Profile</a></li>
+        <li><a href="#" style="color: #000">Wall<span class="sr-only">(current)</span></a></li>
+        <li><a href="pages/profile.php" style="color: #000">Profile</a></li>
       </ul>
 
       <form class="navbar-form navbar-left">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="Cari Teman Kamu">
+          <input type="text" class="form-control" placeholder="Cari Jodoh Kamu">
         </div>
         <button type="submit" class="btn btn-default">Cari</button>
       </form>
@@ -77,7 +77,7 @@
       <ul class="nav navbar-nav navbar-right">
     
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color: #fff;">Akun <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color: #000;">Akun <span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="#">Iklan</a></li>
             <li><a href="#">Setting</a></li>
